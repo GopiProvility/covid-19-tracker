b82a833957897856dc985ce033c3786d981c02fb
umarfarooq95 <mohamedumar931@gmail.com>
2020-05-02 12:58:08 +0530

    render pie chart and column chart with data in home component

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -2,6 +2,7 @@ import {Component, OnInit} from '@angular/core';
 import {GlobalCoronaServiceService} from '../../service/global-corona-service.service';
 import { TotalCoronaCasesInfo } from '../model/totalcorona-casses';
 import { GlobalDataSummary } from '../model/global-data';
+import { GoogleChartInterface } from 'ng2-google-charts';
 
 @Component({
   selector: 'app-home',
@@ -16,12 +17,25 @@ export class HomeComponent implements OnInit {
     totalDeaths: 0,
     totalActive: 0
   };
+
+  globalData: GlobalDataSummary[];
+
+  pieChart: GoogleChartInterface = {
+    chartType: 'PieChart',
+  };
+
+  columnChart: GoogleChartInterface = {
+    chartType: 'ColumnChart',
+  };
+
   constructor(private  globalCoronaServiceService: GlobalCoronaServiceService) {
   }
 
   ngOnInit(): void {
     this.globalCoronaServiceService.getGlobalCoronaData().subscribe({
       next: (data: GlobalDataSummary[]) => {
+
+        this.globalData = data;
 
         data.forEach(row => {
           this.totalCoronaCasesInfo.totalConfirmed += row.confirmed;
@@ -30,8 +44,48 @@ export class HomeComponent implements OnInit {
           this.totalCoronaCasesInfo.totalRecovered += row.recovered;
         });
 
+        this.initCharts();
+
       }
     });
+  }
+
+  private initCharts() {
+    const dataTable = [];
+
+    dataTable.push(['Country', 'Cases']);
+
+    this.globalData.forEach(cs => {
+      dataTable.push([
+        cs.country , cs.confirmed
+      ]);
+    });
+
+    this.buildPieChart(dataTable);
+
+    this.buildColumnChart(dataTable);
+  }
+
+  private buildPieChart(dataTable) {
+    this.pieChart = {
+      chartType: 'PieChart',
+      // tslint:disable-next-line: object-literal-shorthand
+      dataTable: dataTable,
+      options: {
+        height : 600
+      },
+    };
+  }
+
+  private buildColumnChart(dataTable) {
+    this.columnChart = {
+      chartType: 'ColumnChart',
+      // tslint:disable-next-line: object-literal-shorthand
+      dataTable: dataTable,
+      options: {
+        height : 600
+      },
+    };
   }
 
 }
